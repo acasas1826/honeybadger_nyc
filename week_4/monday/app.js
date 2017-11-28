2a6c0dcd9e7ddbd8525b9f6a3d20c55d3b7a6984
Andres Casas <andresdavidcr@hotmail.com>
2017-11-27 20:41:40 -0500

    save

--- week_4/monday/app.js
+++ week_4/monday/app.js
@@ -1,3 +1,39 @@
-console.log(hellooo);
+
+
+var statement = 'HELLO WORLD';
+
+console.log(statement.toUpperCase());
+
+console.log(statement.toLocaleLowerCase());
+
+
+if(1 < 2){
+    console.log ('yayyyyy');
+}
+else if(true || false){
+    console.log('pizaaaaaa')
+}
+else {
+    console.log('tacooos')
+}
+
+
+if(1 < 2){
+    console.log ('yayyyyy');
+}
+if(true || false){
+    console.log('pizaaaaaa')
+}
+else {
+    console.log('tacooos')
+}
+
+
+
+
+
+
+
+
 
 
